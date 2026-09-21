81c144b1b87c66bcbec69c6ebf0f364f87c10688
MatthiewM-sudo <genzo67700@gmail.com>
2026-09-21 03:23:42 +0200

    Fix echo commands in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]; then
-  print "[!] Please run setup script as root (sudo ./setup.sh)"
+  echo "[!] Please run setup script as root (sudo ./setup.sh)"
   exit 1
 fi
 
 BASE_DIR=$(pwd)
 
-print "[*] Compiling C++ network engine..."
+echo "[*] Compiling C++ network engine..."
 cd "$BASE_DIR/FUN/SCANNER" || exit 1
 make clean && make
 
-print "[*] Installing CubeMap global launcher executable into /usr/local/bin..."
+echo "[*] Installing CubeMap global launcher executable into /usr/local/bin..."
 LAUNCHER_PATH="/usr/local/bin/CubeMap"
 
 echo "#!/bin/bash" > "$LAUNCHER_PATH"
@@ -20,4 +20,4 @@ echo "python3 $BASE_DIR/MAIN/Main.py \"\$@\"" >> "$LAUNCHER_PATH"
 chmod +x "$LAUNCHER_PATH"
 chmod +x "$BASE_DIR/MAIN/Main.py"
 
-print "[+] Installation completed. You can now execute 'CubeMap' globally from any path."
+echo "[+] Installation completed. You can now execute 'CubeMap' globally from any path."
